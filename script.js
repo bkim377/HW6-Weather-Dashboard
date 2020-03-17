38ce3997b6d54ff6b930859ebafbf8e256f3ef4e
Brandon Kim <bkim377@gatech.edu>
2020-03-17 19:41:46 -0400

    Checkpoint 11 - added functionality for last searched city upon loading up the page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  // ********** This .js file sets up the page's HTML elements and the search bar's history.
+  // ********** This .js file sets up the page's HTML weather elements and the search bar's history.
 
   // ********** Part 1: Creates the top right card and adds the city name and weather data
 
@@ -17,7 +17,6 @@ $(document).ready(function() {
   wrapper1CityName.attr("id", "city-name");
   var wrapper1CityIcon = $("<img>");
   wrapper1CityIcon.attr("id", "current-icon");
-
 
   // appends all necessary card elements to the top right card
   wrapper1CityName.append("(" + moment().format("MM/DD/YYYY, h:mm:ss a") + ")");
@@ -45,6 +44,7 @@ $(document).ready(function() {
   var wrapper1Row2 = $("<div>"); // Creates the container for the card
   wrapper1Row2.addClass("row");
 
+  // for loop that creates the blue cards for the weekly forecast
   for (var j = 0; j < 5; j++) {
     var row2Card = $("<div>"); // Creates the card itself
     row2Card.addClass("card mx-2 my-2");
@@ -77,40 +77,38 @@ $(document).ready(function() {
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
   // ********** Part 3: Adds the search history beneath the search bar
-  // Code taken from Unit 4 (Web APIs), Exercise 28, solved folder, script.js
+  // Code started from Unit 4 (Web APIs), Exercise 28, solved folder, script.js
 
   cityInput = $("#city-search-bar");
   cityForm = $("#city-form");
   cityList = $("#city-list");
-  var APIkey = "27e7765f93be01e0b5a867f22943cd6a";
 
   var cities = [];
+  var lastEnteredCity = "";
 
   init();
 
   function renderCities() {
     // Clear city element
     cityList.html("");
-
     // Render a new li for each city
     for (var i = 0; i < cities.length; i++) {
       var city = cities[i];
-
-      var li = $("<li>");
-      li.text(city);
-      li.attr("data-index", i);
-
-      var button = $("<button>");
-      button.text("Search");
-
-      li.append(button);
-      cityList.append(li);
+      var list = $("<li>");
+      list.text(city);
+      list.attr("data-index", i);
+    // Adds search button to quickly search that city's weather forecast again
+      var searchButton = $("<button>");
+      searchButton.addClass("past-search");
+      searchButton.text("Search");
+      list.append(searchButton);
+      cityList.append(list);
     }
   }
 
   function init() {
     // Get stored cities from localStorage and parse the JSON string to an object
-    var storedCities = JSON.parse(localStorage.getItem("left-side"));
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
     // If cities were retrieved from localStorage, update the cities array to it
     if (storedCities !== null) {
       cities = storedCities;
@@ -127,10 +125,15 @@ $(document).ready(function() {
   // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
   
   // ********** Part 4: Requests data from OpenWeatherAPI when the search button is clicked
+
+  var APIkey = "27e7765f93be01e0b5a867f22943cd6a";
   // When form is submitted...
   cityForm.on("click", function(event) {
     event.preventDefault();
     var cityText = cityInput.val();
+    lastEnteredCity = cityInput.val();
+    localStorage.setItem("reloadValue", lastEnteredCity);
+
     // Return from function early if submitted cityText is blank
     if (cityText === "" || cityText === $("cities")) {
       return;
@@ -143,15 +146,14 @@ $(document).ready(function() {
     renderCities();
 
     // This part calls the OpenWeather API and retrieves the weather data for the searched city
-    // Code taken from Unit 6, Exercise 5, solved folder, bujumbra-solved.html
-
+    // Code started from Unit 6, Exercise 5, solved folder, bujumbra-solved.html
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityText + "&appid=" + APIkey;
+
     // ++++++++++ AJAX request 1: top-right card (Temperature, Humidity, Wind Speed)   
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // We store all of the retrieved data inside of an object called "response"
       .then(function(response) {
         // Transfer content to HTML (top-right card)
         $("#city-name").html(response.name + " (" + moment().format("MM/DD/YYYY") + ")");
@@ -169,7 +171,7 @@ $(document).ready(function() {
           url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon,
           method: "GET"
         }).then(function(response) {
-          // Adds color to UV Index line based on magnitude 
+          // Adds color to UV Index line based on magnitude - lightgreen, yellow, orange, red, purple
           $("#city-UV").text("UV Index: " + response.value);
           if (response.value < 3) {
             $("#city-UV").addClass("favorable").removeClass("moderate high very-high severe");
@@ -198,23 +200,145 @@ $(document).ready(function() {
         var tempF = (response.list[i].main.temp - 273.15) * 1.8 + 32;
         $("#temp-"+(i+1)).text("Temp: " + tempF.toFixed(2) + " °F");
         $("#humid-"+(i+1)).text("Humidity: " + response.list[i].main.humidity + "%");
-
-    
       }
     });
-    
-
     });
 
-  // When an element inside of the cityList is clicked...
-  cityList.on("click", function(event) {
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+// ********** Part 5: Requests data from OpenWeatherAPI when a green search button is clicked
+// ********** This code is a repeat of part 4's code, just applied to clicking the green search buttons instead of the search bar
+  
+// When an element inside of the cityList is clicked...
+  $(".past-search").on("click", function(event) {
     var element = event.target;
-
     // If that element is a button...
     if (element.matches("button") === true) {
       // *****Still need to do***** Get its data-index value and search that city again
-      // var index = element.parentElement.getAttribute("data-index");
-      // todos.splice(index, 1);
+      var index = element.parentElement.getAttribute("data-index");
+      var cityText = cities[index];
+      lastEnteredCity = cityText;
+      localStorage.setItem("reloadValue", lastEnteredCity);
+
+      var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityText + "&appid=" + APIkey;
+    // ++++++++++ AJAX request 1: top-right card (Temperature, Humidity, Wind Speed)   
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        // Transfer content to HTML (top-right card)
+        $("#city-name").html(response.name + " (" + moment().format("MM/DD/YYYY") + ")");
+        var iconURL = ("https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+        wrapper1CityIcon.attr("src", iconURL);
+        $("#city-humid").text("Humidity: " + response.main.humidity + "%");
+        $("#city-wind").text("Wind Speed: " + response.wind.speed + " mph");
+        // Convert the temp to fahrenheit
+        var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+        // add temp content to html
+        $("#city-temp").text("Temperature: " + tempF.toFixed(2) + " °F");
+
+        // ++++++++++ AJAX request 2: top-right card (UV Index)
+        $.ajax({
+          url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon,
+          method: "GET"
+        }).then(function(response) {
+          // Adds color to UV Index line based on magnitude - lightgreen, yellow, orange, red, purple
+          $("#city-UV").text("UV Index: " + response.value);
+          if (response.value < 3) {
+            $("#city-UV").addClass("favorable").removeClass("moderate high very-high severe");
+          } else if (response.value >= 3 && response.value < 6) {
+            $("#city-UV").addClass("moderate").removeClass("favorable high very-high severe");
+          } else if (response.value >= 6 && response.value < 8) {
+            $("#city-UV").addClass("high").removeClass("favorable moderate very-high severe");
+          } else if (response.value >= 8 && response.value < 11) {
+            $("#city-UV").addClass("very-high").removeClass("favorable moderate high severe");
+          } else if (response.value > 11) {
+            $("#city-UV").addClass("severe").removeClass("favorable moderate high very-high");
+          }
+        });
+      });
+  
+      var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityText + "&cnt=5&appid=" + APIkey;
+    $.ajax({
+      url: queryURLforecast,
+      method: "GET"
+    }).then(function(response) {
+      for (var i = 0; i < 5; i++) {
+        // adds future weather icons to the weekly forecast
+        var iconURL = ("https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
+        $("#future-icons-"+(i+1)).attr("src", iconURL);
+        // adds temperature and humidity lines to weekly forecast
+        var tempF = (response.list[i].main.temp - 273.15) * 1.8 + 32;
+        $("#temp-"+(i+1)).text("Temp: " + tempF.toFixed(2) + " °F");
+        $("#humid-"+(i+1)).text("Humidity: " + response.list[i].main.humidity + "%");
+      }
+    });
     }
   });
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+// ********** Part 6: Requests data from OpenWeatherAPI when the page is (re)loaded
+// ********** This code is a repeat of part 4's code, just applied to refreshing the page instead of the search bar
+
+$(document).ready(function() {
+  var lastCity = localStorage.getItem("reloadValue");
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + lastCity + "&appid=" + APIkey;
+  // ++++++++++ AJAX request 1: top-right card (Temperature, Humidity, Wind Speed)   
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function(response) {
+      // Transfer content to HTML (top-right card)
+      $("#city-name").html(response.name + " (" + moment().format("MM/DD/YYYY") + ")");
+      var iconURL = ("https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+      wrapper1CityIcon.attr("src", iconURL);
+      $("#city-humid").text("Humidity: " + response.main.humidity + "%");
+      $("#city-wind").text("Wind Speed: " + response.wind.speed + " mph");
+      // Convert the temp to fahrenheit
+      var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+      // add temp content to html
+      $("#city-temp").text("Temperature: " + tempF.toFixed(2) + " °F");
+
+      // ++++++++++ AJAX request 2: top-right card (UV Index)
+      $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIkey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon,
+        method: "GET"
+      }).then(function(response) {
+        // Adds color to UV Index line based on magnitude - lightgreen, yellow, orange, red, purple
+        $("#city-UV").text("UV Index: " + response.value);
+        if (response.value < 3) {
+          $("#city-UV").addClass("favorable").removeClass("moderate high very-high severe");
+        } else if (response.value >= 3 && response.value < 6) {
+          $("#city-UV").addClass("moderate").removeClass("favorable high very-high severe");
+        } else if (response.value >= 6 && response.value < 8) {
+          $("#city-UV").addClass("high").removeClass("favorable moderate very-high severe");
+        } else if (response.value >= 8 && response.value < 11) {
+          $("#city-UV").addClass("very-high").removeClass("favorable moderate high severe");
+        } else if (response.value > 11) {
+          $("#city-UV").addClass("severe").removeClass("favorable moderate high very-high");
+        }
+      });
+    });
+
+    var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + lastCity + "&cnt=5&appid=" + APIkey;
+  $.ajax({
+    url: queryURLforecast,
+    method: "GET"
+  }).then(function(response) {
+    for (var i = 0; i < 5; i++) {
+      // adds future weather icons to the weekly forecast
+      var iconURL = ("https://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png");
+      $("#future-icons-"+(i+1)).attr("src", iconURL);
+      // adds temperature and humidity lines to weekly forecast
+      var tempF = (response.list[i].main.temp - 273.15) * 1.8 + 32;
+      $("#temp-"+(i+1)).text("Temp: " + tempF.toFixed(2) + " °F");
+      $("#humid-"+(i+1)).text("Humidity: " + response.list[i].main.humidity + "%");
+    }
+  });
+
+  });
+
 });
